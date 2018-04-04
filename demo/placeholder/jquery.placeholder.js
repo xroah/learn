01d263dd24fe30d01ed5aaafcd6e0dcd7c084176
xroah <396069583@qq.com>
2018-04-04 11:16:18 +0800

    placeholder,时间代理

--- demo/placeholder/jquery.placeholder.js
+++ demo/placeholder/jquery.placeholder.js
@@ -12,17 +12,20 @@
     }
 
     function initEvent(el) {
-        $(el).on("focus", function () {
-            var _this = $(this);
+        //focus,blur时间不支持冒泡
+        $(document.body).on("focusin", function (evt) {
+            var _this = $(el);
+            if (evt.target !== el) return;
             if (!isSurport() && _this.val() === _this.attr("placeholder")) {
                 _this.val("").css("color", _this.data("origColor"));
             }
-          }).on("blur", function () {
-            var _this = $(this);
+          }).on("focusout", function (evt) {
+            var _this = $(el);
+            if (evt.target !== el) return;
             if (!isSurport() && !_this.val()) {
                 _this.val(_this.attr("placeholder")).css("color", styles.color);
             }
-        });
+        }).attr("tabIndex", 0);
     }
 
     $.fn.extend({
@@ -32,10 +35,10 @@
                     color = _this.css("color"),
                     placeholder = _this.attr("placeholder");
                 _this.data("origColor", color);
-                if (!_this.val() && !isSurport()) {
-                    _this.val(placeholder).css("color", styles.color);
+                if (!isSurport()) {
+                    initEvent(this);
+                    !_this.val() && _this.val(placeholder).css("color", styles.color);
                 }
-                initEvent(this);
             });
         }
     });
