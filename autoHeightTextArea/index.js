f5bebb9984276f3a45dc4dd9dd5940e3e440c07d
xroah <396069583@qq.com>
2018-04-20 11:18:32 +0800

    火狐浏览器下高度计算错误修复

--- autoHeightTextArea/index.js
+++ autoHeightTextArea/index.js
@@ -1,56 +1,59 @@
-!function() {
+! function () {
     var doc = document;
+
     function AutoHeightTextArea(el) {
         this.el = el;
         this.size = {};
     }
     var fn = AutoHeightTextArea.prototype;
 
-    fn.initSize = function() {
-        var el = $(this.el);
+    fn.initSize = function () {
+        var el = $(this.el),
+            style = getComputedStyle(this.el);
         this.size.padding = {
-            top: parseFloat(el.css("padding-top")),
-            bottom: parseFloat(el.css("padding-bottom")),
-            left: parseFloat(el.css("padding-left")),
-            right: parseFloat(el.css("padding-right"))
+            top: parseFloat(style["padding-top"]),
+            bottom: parseFloat(style["padding-bottom"]),
+            left: parseFloat(style["padding-left"]),
+            right: parseFloat(style["padding-right"])
         };
         this.size.orignalHeight = el.height();
         this.size.orignalWidth = el.width();
         this.size.border = {
-            top: parseFloat(el.css("border-top-width")),
-            bottom: parseFloat(el.css("border-bottom-width")),
-            left: parseFloat(el.css("border-left-width")),
-            right: parseFloat(el.css("border-right-width")) 
+            top: parseFloat(style["border-top-width"]),
+            bottom: parseFloat(style["border-bottom-width"]),
+            left: parseFloat(style["border-left-width"]),
+            right: parseFloat(style["border-right-width"])
         };
-        this.fontSize = el.css("font-size");
-        this.fontFamily = el.css("font-family");
-        this.boxSizing = el.css("box-sizing");
-        this.lineHeight = el.css("line-height");
+        this.fontSize = style["font-size"];
+        this.fontFamily = style["font-family"];
+        this.boxSizing = style["box-sizing"];
+        this.lineHeight = style["line-height"];
         return this;
     };
 
-    fn.computeHeight = function(value) {
+    fn.computeHeight = function (value) {
+        //生成一个隐藏的textarea,复制当前textarea的属性
+        //计算隐藏textarea的高度，设置到当前textarea上
         var ta = doc.createElement("textarea"),
-            width = this.size.orignalWidth, 
-            height;
+            width = this.size.orignalWidth,
+            height = this.size.orignalHeight,
+            padding = this.size.padding,
+            border = this.size.border;
         if (this.boxSizing === "border-box") {
-            width += this.size.padding.left + this.size.padding.right + this.size.border.left + this.size.border.right;
+            width += padding.left + padding.right + border.left + border.right;
+            height += padding.top + padding.bottom + border.top + border.bottom;
         }
         $(ta).val(value).css({
             "position": "absolute",
             "visibility": "hidden",
-            "overflow": "hidden",
             "font-size": this.fontSize,
             "font-family": this.fontFamily,
             "box-sizing": this.boxSizing,
             "z-index": -1000,
             "width": width,
-            "height": 0,
-            "padding-left": this.size.padding.left,
-            "padding-right": this.size.padding.right,
-            "padding-top": this.size.padding.top,
-            "padding-bottom": this.size.padding.bottom,
-            "line-height": this.lineHeight
+            "height": height,
+            "padding": getComputedStyle(this.el).getPropertyValue("padding"),
+            "border": getComputedStyle(this.el).getPropertyValue("border")
         });
         $(doc.body).append(ta);
         height = ta.scrollHeight; //scrollHeight不包含边框
@@ -58,22 +61,26 @@
         return height;
     };
 
-    fn.setHeight = function() {
+    fn.setHeight = function () {
         var height = this.computeHeight(this.el.value);
         if (this.boxSizing === "content-box") {
-            height = height - this.size.padding.top - this.size.padding.bottom;   
+            height = height - this.size.padding.top - this.size.padding.bottom;
         }
         $(this.el).css("height", height);
         return this;
     };
 
-    fn.initEvent = function() {
-        $(this.el).on("input propertychange", $.proxy(this.setHeight, this)).on("keydown", function(evt) {
-            var code = evt.keyCode;
-            //ie9下按下backspace,delete键不会触发input事件
+    fn.initEvent = function () {
+        $(this.el).on("input propertychange", $.proxy(this.setHeight, this)).on("keydown", function (evt) {
+            var code = evt.keyCode,
+                _this = $(this);
             if (code === 8 || code === 46) {
-                $(this).trigger("input");
-            } 
+                //ie9下按下backspace,delete键不会触发input事件
+                //如果当前删除最后一行的最后一个字符，并不会立即删除最后一行(最后一行是空白行)
+                setTimeout(function () {
+                    _this.trigger("input");
+                });
+            }
             //如果没有输入任何则不做处理(ie下bug)
             if (code === 13 && !$.trim(this.value)) {
                 evt.preventDefault();
@@ -82,12 +89,12 @@
         return this;
     };
 
-    fn.init = function() {
+    fn.init = function () {
         this.initEvent().initSize().setHeight();
     };
     $.fn.extend({
-        autoHeightTextArea: function(fn) {
-            this.each(function() {
+        autoHeightTextArea: function (fn) {
+            this.each(function () {
                 var nodeName = this.nodeName.toLowerCase();
                 if (nodeName === "textarea") {
                     new AutoHeightTextArea(this).init();
