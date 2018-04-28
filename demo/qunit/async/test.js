e37fe9c28b9d356174b7093271f1b47d1a833a3a
xroah <396069583@qq.com>
2018-04-28 17:42:53 +0800

    qunit学习

---
+++ demo/qunit/async/test.js
@@ -0,0 +1,8 @@
+QUnit.test( "asynchronous test: async input focus", function( assert ) {
+  var done = assert.async();
+  var input = $( "#test-input" ).focus();
+  setTimeout(function() {
+    assert.equal( document.activeElement, input[0], "Input was focused" );
+    done();
+  });
+});
