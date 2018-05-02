ad25e32b2dfe967c436849edd04f9f02d2b55571
xroah <396069583@qq.com>
2018-05-02 16:01:06 +0800

    qunit module

--- demo/qunit/async/test.js
+++ demo/qunit/async/test.js
@@ -1,3 +1,12 @@
+QUnit.module("core", {
+  before: function() {
+    console.log("test started");
+  },
+  after: function() {
+    console.log("test ended");
+  }
+});
+
 QUnit.test( "asynchronous test: async input focus", function( assert ) {
   var done = assert.async();
   var input = $( "#test-input" ).focus();
