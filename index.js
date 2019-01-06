238b81ff19374cb871fb2d152d50bfb51a345814
xroah <396069583@qq.com>
2019-01-06 22:03:08 +0800

    add move functions

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const {rm, rmSync} = require("./lib/rm");
+const {cp, cpSync} = require("./lib/cp");
+
+function mv(src, dest, options, callback) {
+    if (typeof options === "function") {
+        callback = options;
+        options = {};
+    }
+    cp(src, dest, options, err => {
+        console.log("........", err)
+        if (err) return callback(err);
+        rm(src, options, callback);
+    });
+}
+
+function mvSync(src, dest, options) {
+    cpSync(src, dest, options);
+    rmSync(src, options);
+}
 
 module.exports = {
     rm,
-    rmSync
+    rmSync,
+    cp,
+    cpSync,
+    mv,
+    mvSync
 };
