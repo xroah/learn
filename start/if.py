f393abbaeb3f539e5c91fcbd715fa7485715f9d6
xroah <396069583@qq.com>
2019-07-07 20:18:03 +0800

    update

---
+++ start/if.py
@@ -0,0 +1,15 @@
+list = []
+
+if list:
+    print(list)
+else:
+    print("list is empty")
+
+list = [value for value in range(1, 50)]
+
+for item in list:
+    if item > 10 and item % 2 == 0:
+        print(item)
+    elif item % 3 == 0 or item % 5 == 0:
+        print(f"item={item}")
+
