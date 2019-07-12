ebf63a4ec8911c8cdea4365ccc74fd87201465c6
xroah <396069583@qq.com>
2019-07-12 21:15:02 +0800

    update

--- start/function.py
+++ start/function.py
@@ -1,10 +1,11 @@
-def greet_user(username):
+def greet_user(username = "mary"):
     """Simple function"""
     print(f"Hello {username.title()}")
 
 def print_info(name, age):
     print(f"name={name}, age={age}")
 
+greet_user()
 greet_user("jack")
 print_info(20, "Jack")
 print_info(age = 20, name = "Jack")
