befbf298ff6ae48283dbd6ae1ef3529f2fa6c115
xroah <396069583@qq.com>
2019-07-13 13:09:29 +0800

    update

--- start/function.py
+++ start/function.py
@@ -5,6 +5,9 @@ def greet_user(username = "mary"):
 def print_info(name, age):
     print(f"name={name}, age={age}")
 
+def sum(a, b):
+    return a + b
+
 greet_user()
 greet_user("jack")
 print_info(20, "Jack")
@@ -12,3 +15,5 @@ print_info(age = 20, name = "Jack")
 
 #TypeError: print_info() missing 2 required positional arguments: 'name' and 'age':
 #print_info()
+
+print(sum(1, 1))
