f9dcc849294f85fd6703dde23c1ae9ce968cf9d7
xroah <396069583@qq.com>
2019-07-14 19:03:51 +0800

    update

---
+++ start/function-args.py
@@ -0,0 +1,12 @@
+def sum(*numbers):
+    print(numbers)
+    
+    ret = 0
+
+    for n in numbers:
+        ret +=n
+    
+    return ret
+
+print(f"1 + 1 = {sum(1, 1)}")
+print(f"1 + 2 + 3 = {sum(1, 2, 3)}")
