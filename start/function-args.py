333ab5821b2dad0039dbe4b6d9e17d32c1bf5c2d
xroah <396069583@qq.com>
2019-07-14 19:36:07 +0800

    update

--- start/function-args.py
+++ start/function-args.py
@@ -11,3 +11,13 @@ def sum(*numbers):
 print(sum())
 print(f"1 + 1 = {sum(1, 1)}")
 print(f"1 + 2 + 3 = {sum(1, 2, 3)}")
+
+def build_profile(first_name, last_name, **user_info):
+    user_info["first_name"] = first_name
+    user_info["last_name"] = last_name
+
+    return user_info
+
+print(
+    build_profile("albert", "einstein", location="princeton", field="physics")
+)
