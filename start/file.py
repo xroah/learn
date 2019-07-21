c060f11baed50b584e56c604cf4e7729eb5b81a6
xroah <396069583@qq.com>
2019-07-21 11:04:05 +0800

    update

--- start/file.py
+++ start/file.py
@@ -1,3 +1,8 @@
 with open("./class.py") as file_object:
     content = file_object.read()
 print(content)
+
+
+with open("./tuples.py") as file_object:
+    for line in file_object:
+        print(line.rstrip())
