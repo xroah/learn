f6d2a9c371344e49b6be764e43af60db1aa8726f
xroah <396069583@qq.com>
2019-07-25 21:15:07 +0800

    update

---
+++ start/exception.py
@@ -0,0 +1,16 @@
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can not divide by zero")
+
+a = input("Please input dividend: ")
+b = input("Please input divisor: ")
+
+try:
+    a = float(a)
+    b = float(b)
+    q = a / b
+except Exception as e:
+    # print("Error", e)
+else:
+    print("result=%s" % q)
