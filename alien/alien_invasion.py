b110ca665b2c74b20f686204540621f48dedc6e6
xroah <396069583@qq.com>
2019-08-27 21:41:47 +0800

    update

--- alien/alien_invasion.py
+++ alien/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -10,16 +11,18 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (230, 230, 230)
 
+        self.ship = Ship(self)
+
     def run_game(self):
         while True:
             for event in pygame.event.get():
-                if event.type === pygame.QUIT:
+                if event.type == pygame.QUIT:
                     sys.exit()
             
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
-
-if __name__ === "__main__":
+if __name__ == "__main__":
     ai = AlienInvasion()
     ai.run_game()
