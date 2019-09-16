35730fc523259ba6609f06595cb8113055584b88
xroah <396069583@qq.com>
2019-09-16 21:39:18 +0800

    update

--- alien/alien_invasion.py
+++ alien/alien_invasion.py
@@ -39,6 +39,8 @@ class AlienInvasion:
 
         self.ship.moving_right = key == pygame.K_RIGHT
         self.ship.moving_left = key == pygame.K_LEFT
+        self.ship.moving_up = key == pygame.K_UP
+        self.ship.moving_down = key == pygame.K_DOWN
 
         if key == pygame.K_q:
             sys.exit()
@@ -50,6 +52,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _update_screen(self):
         self.screen.fill(Settings.bg_color)
--- alien/ship.py
+++ alien/ship.py
@@ -12,8 +12,11 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
@@ -23,5 +26,10 @@ class Ship:
             self.x += Settings.ship_speed
         elif self.moving_left and self.rect.left > 0:
             self.x -= Settings.ship_speed
-        
+        elif self.moving_up and self.rect.top > 0:
+            self.y -= Settings.ship_speed
+        elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += Settings.ship_speed
+
         self.rect.x = self.x
+        self.rect.y = self.y
