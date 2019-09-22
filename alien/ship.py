fbe08db158fb375ccbf1dc51b048db12eb664378
xroah <396069583@qq.com>
2019-09-22 23:31:52 +0800

    image path

--- alien/ship.py
+++ alien/ship.py
@@ -1,13 +1,16 @@
 import pygame
 from settings import Settings
+import os
 
 
 class Ship:
     def __init__(self, ai_game):
+        image_dir = os.path.dirname(__file__)
+
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load(os.path.join(image_dir, "./images/ship.bmp"))
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
