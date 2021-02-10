e357939ab0661664c88fa816b7737f86ef10d54b
xroah <396069583@qq.com>
2021-02-10 22:59:22 +0800

    remove

--- alien/__main__.py
+++
@@ -1,4 +0,0 @@
-from alien_invasion import AlienInvasion
-
-if __name__ == "__main__":
-    AlienInvasion()
--- alien/screen.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from settings import Settings
-
-screen = pygame.display.set_mode((
-    Settings.screen_width,
-    Settings.screen_height
-))
