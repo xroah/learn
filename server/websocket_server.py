1d8cecee267b78fcb70e5301ffcb288b9313c797
xroah <396069583@qq.com>
2021-08-28 22:59:08 +0800

    websocket reply

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -1,9 +1,17 @@
 import asyncio
 import websockets
+import time
 
 
 async def _start(websocket, path):
+    mark = "*" * 20
+
     await websocket.send("Hello")
+
+    async for msg in websocket:
+        print(f"{mark}{msg}{mark}")
+
+        await websocket.send(f"received: {int(time.time())}")
 
 
 def start_server():
